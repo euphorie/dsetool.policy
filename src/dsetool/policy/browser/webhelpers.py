8e6f487ae461f0442373da5ece7d8beb5b9a757b
Alexander Pilz <pilz@syslab.com>
2025-08-28 14:59:12 +0200

    fetch language via api and make about_urls a class var

--- src/dsetool/policy/browser/webhelpers.py
+++ src/dsetool/policy/browser/webhelpers.py
@@ -1,4 +1,5 @@
 from osha.oira.client.browser.webhelpers import OSHAWebHelpers
+from plone import api
 from plone.memoize.instance import memoize
 
 
@@ -6,6 +7,20 @@ class DSEToolWebHelpers(OSHAWebHelpers):
     """Browser view with utility methods that can be used in templates.
     View name: @@webhelpers
     """
+
+    about_urls = {
+        "en": "/dangerous-substances/about-e-tool.html",
+        "no": "/dangerous-substances/no/om-e-verktøyet.html",
+        "is": "/dangerous-substances/is/um-rafræna-tólið.html",
+        "pt": "/dangerous-substances/pt/acerca-da-ferramenta-eletrónica.html",
+        "sl": "/dangerous-substances/sl/o-e-orodju.html",
+        "et": "/dangerous-substances/et/teave-e-vahendi-kohta.html",
+        "de_AT": "/dangerous-substances/AT_de/über-das-e-tool.html",
+        "ro": "/dangerous-substances/ro/despre-instrumentul-electronic.html",
+        "de": "/dangerous-substances/de/über-das-e-tool.html",
+        "es": "/dangerous-substances/es/acerca-de-la-herramienta-electrónica.html",
+        "lt": "/dangerous-substances/lt/apie-epriemonę.html",
+    }
 
     @property
     @memoize
@@ -29,17 +44,6 @@ class DSEToolWebHelpers(OSHAWebHelpers):
 
     def get_about_url(self):
         """mapper of language to about url"""
-        about_urls = {
-            "en": "/dangerous-substances/about-e-tool.html",
-            "no": "/dangerous-substances/no/om-e-verktøyet.html",
-            "is": "/dangerous-substances/is/um-rafræna-tólið.html",
-            "pt": "/dangerous-substances/pt/acerca-da-ferramenta-eletrónica.html",
-            "sl": "/dangerous-substances/sl/o-e-orodju.html",
-            "et": "/dangerous-substances/et/teave-e-vahendi-kohta.html",
-            "de_AT": "/dangerous-substances/AT_de/über-das-e-tool.html",
-            "ro": "/dangerous-substances/ro/despre-instrumentul-electronic.html",
-            "de": "/dangerous-substances/de/über-das-e-tool.html",
-            "es": "/dangerous-substances/es/acerca-de-la-herramienta-electrónica.html",
-            "lt": "/dangerous-substances/lt/apie-epriemonę.html",
-        }
-        return about_urls.get(self.request.locale.getLocaleID(), about_urls["en"])
+        return self.about_urls.get(
+            api.portal.get_current_language(), self.about_urls["en"]
+        )
