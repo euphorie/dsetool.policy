5b18b4d611110256e8bb55c3805267e06474949e
Syslab.com Developers <deve@syslab.com>
2026-03-11 15:30:52 +0100

    Add @@recommendations-dictionary.pdf view
    Refs: https://github.com/syslabcom/scrum/issues/4507

---
+++ src/dsetool/policy/browser/recommendations_dictionary.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+from euphorie.client import MessageFactory as _
+from euphorie.client import utils
+from euphorie.client.browser.pdf import PdfView
+from euphorie.client.browser.report import ReportInventory
+from plone import api
+from plone.memoize.view import memoize
+from urllib.parse import quote
+
+
+class RecommendationsDictionary(ReportInventory):
+    """Instead of generating the Report Inventory which shows the results of a
+    particular session, we display all the recommendations for all modules.
+    """
+
+    def get_all_options(self, module):
+        for choice in module.values():
+            for option in choice.values():
+                yield option
+
+    @memoize
+    def modules(self):
+        return [
+            module
+            for module in self.context.values()
+            if module.title != "label_custom_risks"
+        ]
+
+
+class RecommendationsDictionaryPDF(PdfView):
+    """Re-use the parts from the @@pdf view to generate a PDF file from a view."""
+
+    def __call__(self):
+        context = self.context
+        view = api.content.get_view("recommendations-dictionary", context, self.request)
+        pdf = self.view_to_pdf(view)
+        filename = f"Recommentations Dictionary - {context.title}.pdf"
+
+        context.REQUEST.RESPONSE.setHeader("Content-Type", "application/pdf")
+        context.REQUEST.RESPONSE.setHeader(
+            "Content-Disposition", "attachment;filename*=UTF-8''%s" % quote(filename)
+        )
+        return pdf
